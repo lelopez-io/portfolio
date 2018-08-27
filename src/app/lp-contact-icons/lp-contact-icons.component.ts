4ce98bb78fdb327cece44396241cb118ff639669
lelopez <ux.lelopez@gmail.com>
2018-08-27 15:32:23 -0500

    attempt vanilla tilt.js

--- src/app/lp-contact-icons/lp-contact-icons.component.ts
+++ src/app/lp-contact-icons/lp-contact-icons.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { VanillaTilt } from 'vanilla-tilt';
+
 @Component({
   selector: 'app-lp-contact-icons',
   templateUrl: './lp-contact-icons.component.html',
@@ -10,6 +12,18 @@ export class LpContactIconsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    const li: NodeListOf<HTMLElement> = document.querySelectorAll('[data-tilt]');
+    const liArray: HTMLElement[] = Array.prototype.slice.call(li);
 
+
+    VanillaTilt.init( liArray, {
+      perspective: 500,
+      max: 20,
+      speed: 400,
+      glare: true,
+      scale: 1.1,
+      'max-glare': 0.5,
+    });
+
+  }
 }
